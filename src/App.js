27b9b4cf5220cc57f3bdd1b12e7bb3bf9d45be30
techybams <bamideleolaoluwa95@gmail.com>
2025-01-01 15:43:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Balance from "./components/Balance";
+
+function App() {
+  return (
+    <div className="bg-slate-300 h-screen">
+      <Balance />
+    </div>
+  );
+}
+
+export default App;
